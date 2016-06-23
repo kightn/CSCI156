a788a5e20ce3c977c4226bd8b5dc11f798f12a30
Natalie Kight <kightn@uindy.edu>
2016-06-23 18:02:20 -0400

    Add files via upload

---
+++ Plot (1).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "Plot.h" 
+
+
+int main()
+{
+	Plot Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones;
+  	names[5] = {Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones};
+	Joe_Brown.setAll( 6, 5);
+	Bob_Smith.setAll( 2, 4);
+	Mary_Smith.setAll( 2, 5);
+	Mark_Jones.setAll( 3, 6);
+	Nancy_Jones.setAll(3, 7);
+	
+cout << "Enter Name (FirstName_LastName) to find row and plot #s:" << endl;
+	cin >> a;
+	
+	return 0;
+}
