c9b6f8e40d21d33296c1a053a20c3a2a4d20c673
Natalie Kight <kightn@uindy.edu>
2016-06-23 18:00:28 -0400

    Add files via upload

--- Plot.cpp
+++ Plot.cpp
@@ -4,20 +4,16 @@
 
 int main()
 {
-  Plot Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones;
+	Plot Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones;
+  	names[5] = {Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones};
 	Joe_Brown.setAll( 6, 5);
 	Bob_Smith.setAll( 2, 4);
 	Mary_Smith.setAll( 2, 5);
 	Mark_Jones.setAll( 3, 6);
 	Nancy_Jones.setAll(3, 7);
 	
-	graves[5] = {Joe_Brown, Bob_Smith, Mary_Smith, Mark_Jones, Nancy_Jones};
-
-for( int k = 0; k <= 4; k++){
-	cout << "Name: " << graves[k] << endl;
-	cout << "Row: " << graves[k].getRow() << endl;
-	cout << "Plot #: " << graves[k].getPlot() << endl;
-}
+cout << "Enter Name (FirstName_LastName) to find row and plot #s:" << endl;
+	cin >> a;
 	
 	return 0;
 }
